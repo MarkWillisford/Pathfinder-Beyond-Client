d1aa413813d44ad127f8b89b440c37aa44740262
Mark Willisford <MarkWillisford@users.noreply.github.com>
2019-05-15 15:45:24 -0700

    fixing heroku

--- src/config/main.config.js
+++ src/config/main.config.js
@@ -1,6 +1,2 @@
-module.exports = {
-  PORT: process.env.PORT || 8080,
-  // other stuff
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:8080/api"
-};
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ||
+"http://localhost:8080/api";
